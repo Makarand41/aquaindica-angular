8dacd9d3f15507f119783af38b7f9a90959c45a5
Makarand41 <makarandpatil67@gmail.com>
2026-04-25 12:30:49 +0530

    again change apiUrl for ngrok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://a38b-2409-40c0-33-f6e1-ddf8-a51c-3f04-4cbf.ngrok-free.app'
+  // apiUrl: 'http://localhost:8080',  // local
+  apiUrl: 'https://9e92-2409-40c0-33-f6e1-ddf8-a51c-3f04-4cbf.ngrok-free.app'
 };
-
